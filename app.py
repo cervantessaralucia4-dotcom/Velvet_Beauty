5ce0e8207817efd130d8e63657c317ec0bad4db3
Sara Cervantes <cervantessaralucia4@gmail.com>
2026-05-13 16:45:33 -0500

    Se creo el panel admin

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
 from flask import Flask, render_template
 from flask_mysqldb import MySQL
+import os
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
+
+UPLOAD_FOLDER = 'static/uploads'
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 # CONFIG MYSQL
 app.config['MYSQL_HOST'] = 'localhost'
@@ -28,5 +34,79 @@ def home():
         products=products
         )
 
+@app.route('/admin/add-product')
+def add_product():
+
+    cursor = mysql.connection.cursor()
+
+    cursor.execute("SELECT * FROM categories")
+
+    categories = cursor.fetchall()
+
+    cursor.close()
+
+    return render_template(
+        'admin/add_product.html',
+        categories=categories
+    )
+
 if __name__ == '__main__':
     app.run(debug=True)
+
+from flask import request, redirect
+
+@app.route('/save-product', methods=['POST'])
+def save_product():
+
+    name = request.form['name']
+    brand = request.form['brand']
+    description = request.form['description']
+    price = request.form['price']
+    stock = request.form['stock']
+    category_id = request.form['category_id']
+
+    image = request.files['image']
+
+    filename = secure_filename(image.filename)
+
+    image_path = os.path.join(
+        app.config['UPLOAD_FOLDER'],
+        filename
+    )
+
+    image.save(image_path)
+
+    db_path = f"uploads/products/{filename}"
+
+    cursor = mysql.connection.cursor()
+
+    query = """
+        INSERT INTO products(
+            name,
+            brand,
+            description,
+            price,
+            stock,
+            category_id,
+            main_image
+        )
+        VALUES(%s,%s,%s,%s,%s,%s,%s)
+    """
+
+    values = (
+        name,
+        brand,
+        description,
+        price,
+        stock,
+        category_id,
+        db_path
+    )
+
+    cursor.execute(query, values)
+
+    mysql.connection.commit()
+
+    cursor.close()
+
+    return redirect('/')
